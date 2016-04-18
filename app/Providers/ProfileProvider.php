0e07e8c454debe15f19ad15e28cbff14711f4927
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-04-18 18:01:37 +0200

    Solution to binding problem

---
+++ app/Providers/ProfileProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProfileProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        app::bind(\App\Profile::class, \App\ProfileHTML::class);
+    }
+}
