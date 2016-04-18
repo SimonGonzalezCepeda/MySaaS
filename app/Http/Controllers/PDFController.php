ea3db3f008fcf4799001afb22ccb18b721bd8fe5
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-04-18 15:57:30 +0200

    Begining to work with events

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use DOMPDF;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Illuminate\Http\Response;
+
+
 
 /**
  * Class PDFController
@@ -17,9 +20,13 @@ class PDFController extends Controller
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function incoiceHtml()
+    public function invoiceHtml()
     {
-        return view('receip');
+        $data = [
+            'vendor' => 'HOLA',
+            'user' => 'Sylver'
+        ];
+        return view($data);
     }
 
     /**
@@ -47,12 +54,18 @@ class PDFController extends Controller
     public function donwload($pdf)
     {
         $filename = "hola.pdf";
-
         return new Response($pdf, 200,[
            'Content-Description' => 'File Transfer',
             'Content-Disposition' => 'filename="'.$filename.'"', //amb attachment, linea per a fer descarregar el fitxer
             'Content-Transfer_Encoding' => 'binary',
             'Content-Type' => 'application/pdf'
         ]);
+    }
+
+    public function view(array $data)
+    {
+        return View::make('cashier::receipt', array_merge(
+            $data, ['invoice' => $this, 'user' => $this->user, 'vendor' => $this->vendor]
+        ));
     }
 }
